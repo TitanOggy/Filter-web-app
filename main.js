6116b278f8bd44989de9b6665025ef1ed8a6d70e
TitanOggy <139459200+TitanOggy@users.noreply.github.com>
2023-07-13 16:34:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+
+
+
+}
+
+function setup(){
+
+    canvas=createCanvas(300, 300);
+    canvas.center();
+
+}
+
+function draw(){
+
+
+
+}
+
+function take_snapshot(){
+
+    save('my photo.png');
+
+}
